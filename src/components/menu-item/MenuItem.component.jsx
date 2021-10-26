d5b8a74068bbbda4fd74b4942a16cdb030a4db61
rahalrazika <rahalraika@gmail.com>
2021-10-26 13:26:59 +0200

    add the hover effect

--- src/components/menu-item/MenuItem.component.jsx
+++ src/components/menu-item/MenuItem.component.jsx
@@ -3,17 +3,15 @@ import './menu-item.styles.scss';
 
 export default function MenuItem({ title, imageUrl, size}) {
   return (
-   
-      <div
-      style={{
+     
+      <div className={`${size} menu-item`}>
+       <div className="background-image" style={{
         background: `url(${imageUrl})`
-      }}
-       className={`${size} menu-item`}>
+      }} ></div>
         <div className="content">
           <h1 className="title">{title.toUpperCase()}</h1>
           <span className="subtitle">Shop Now</span>
         </div>
       </div>
-
   );
 }
