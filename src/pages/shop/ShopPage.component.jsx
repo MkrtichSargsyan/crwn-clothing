9f52ec0fee6019215e3183c05dae815fbbef2c11
rahalrazika <rahalraika@gmail.com>
2021-10-30 18:01:55 +0200

    add the shopt data

---
+++ src/pages/shop/ShopPage.component.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class ShopPage extends Component {
+   state = {
+       collections: 
+      
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ShopPage
