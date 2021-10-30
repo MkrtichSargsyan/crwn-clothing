21894d1c39518a53b71339e4c1cf6cc24798b7cd
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-10-30 23:06:43 +0400

    Create Sign in component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/Header.component';
 import HomePage from './pages/homepage/HomePage.component';
 import ShopPage from './pages/shop/ShopPage.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp.component';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route path={'/signin'} component={SignInAndSignUp}></Route>
       </Switch>
     </div>
   );
