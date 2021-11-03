29db22dbc038ac3fcb27a11ae22999d3a19e36e5
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-11-03 23:25:15 +0400

    Add sign up to signinandsignup page

--- src/pages/sign-in-and-sign-up/SignInAndSignUp.component.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUp.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/SignIn.component';
+import SignUp from '../../components/sign-up/SignUp.component';
+
+import './sign-in-and-sign-up.styles.scss';
 
 function SignInAndSignUp() {
   return (
     <div className={'sign-in-and-sign-up'}>
       <SignIn />
+      <SignUp />
     </div>
   );
 }
