feb6e717f8f8d52164cd72632c16df51bc059a36
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-12-09 09:24:50 +0400

    Add clearItem functionality

--- src/components/checkout-item/CheckoutItem.component.jsx
+++ src/components/checkout-item/CheckoutItem.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './checkout-item.styles.scss';
 
-function CheckoutItem({ cartItem: { name, imageUrl, price, quantity } }) {
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
+
+function CheckoutItem({ cartItem, clearItemFromCart }) {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -10,9 +14,9 @@ function CheckoutItem({ cartItem: { name, imageUrl, price, quantity } }) {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove">&#10005;</div>
+      <div className="remove-button" onClick={()=>clearItemFromCart(cartItem)}>&#10005;</div>
     </div>
   );
 }
 
-export default CheckoutItem;
+export default connect(null, { clearItemFromCart })(CheckoutItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -18,6 +18,13 @@ const cartReducer = (state = initialState, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (item) => item.id !== action.payload.id
+        ),
+      };
 
     default:
       return state;
