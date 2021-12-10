e63c3445ef00468c8d10d3daa180ad72b5216eee
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-12-10 12:47:12 +0400

    Separate collections overview component from shop page

---
+++ src/components/collections-overview/CollectionsOverview.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import PreviewCollection from '../preview-collection/PreviewCollection.component';
+
+import { selectCollections } from '../../redux/shop/shop.selectors.js';
+
+import './collections-overview.styles.scss';
+
+function CollectionsOverview({ collections }) {
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <PreviewCollection key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+}
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/ShopPage.component.jsx
+++ src/pages/shop/ShopPage.component.jsx
@@ -1,22 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import PreviewCollection from '../../components/preview-collection/PreviewCollection.component';
-
-import { selectCollections } from '../../redux/shop/shop.selectors.js';
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview.component';
 
 function ShopPage({ collections }) {
   return (
     <div className={'shop-page'}>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <PreviewCollection key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 }
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
