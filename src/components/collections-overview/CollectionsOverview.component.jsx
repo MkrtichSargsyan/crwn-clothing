60e2305309cd660141a6499126ea4f76ab8d23d7
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-12-10 17:07:35 +0400

    Create selectCollectionsForPreview selector, convert it into array

--- src/components/collections-overview/CollectionsOverview.component.jsx
+++ src/components/collections-overview/CollectionsOverview.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import PreviewCollection from '../preview-collection/PreviewCollection.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors.js';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors.js';
 
 import './collections-overview.styles.scss';
 
@@ -18,7 +18,7 @@ function CollectionsOverview({ collections }) {
 }
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
