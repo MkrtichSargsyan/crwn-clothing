d04e865cc05d1572fc2e65e1efdb234e368b138b
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-12-11 11:59:50 +0400

    Add stripe payment functionality

---
+++ src/components/stripe-button/StripeButton.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+function StripeCheckoutButton({ price }) {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_wpUf9uQAyev6JHkUkyuQWT8000t0d10Rkt';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://cdn.imgbin.com/13/21/10/imgbin-emoji-crown-symbol-computer-icons-emoji-Aye75aHMcwDAqk7QEdkgaEC0H.jpg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+}
+
+export default StripeCheckoutButton;
--- src/pages/checkout/Checkout.component.jsx
+++ src/pages/checkout/Checkout.component.jsx
@@ -8,6 +8,7 @@ import {
   selectCartTotal,
 } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/CheckoutItem.component';
+import StripeCheckoutButton from '../../components/stripe-button/StripeButton.component';
 
 function Checkout({ cartItems, total }) {
   return (
@@ -32,9 +33,13 @@ function Checkout({ cartItems, total }) {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <div className="total">
-        <span>TOTAL: ${total}</span>
+      <div className="total">TOTAL: ${total}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
       </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 }
